cfc382aae036d0101165c861853cb7f428064b67
Chris Soderquist <contact@dakkra.com>
2023-07-23 23:08:18 -0600

    Update website

--- hs-web/src/App.js
+++ hs-web/src/App.js
@@ -1,43 +1,164 @@
 import * as React from 'react';
-import {Box, Container, CssBaseline, Typography} from "@mui/material";
+import {Box, Button, Card, Container, createTheme, CssBaseline, Link, ThemeProvider, Typography} from "@mui/material";
+import {useState} from "react";
+
+export const componentOverrides = {
+    components: {
+        MuiLink: {
+            defaultProps: {
+                underline: 'none',
+            }
+        },
+        MuiCard: {
+            defaultProps: {
+                raised: true
+            },
+            styleOverrides: {
+                root: {
+                    borderRadius: "1cm",
+                    marginTop: "1cm",
+                    padding: "1cm"
+                }
+            }
+        }
+    }
+}
+
+export const lightThemeOptions = {
+    ...componentOverrides,
+    palette: {
+        mode: 'light',
+        primary: {
+            main: '#8061ea',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+    },
+};
+
+export const darkThemeOptions = {
+    ...componentOverrides,
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#83bb67',
+        },
+        secondary: {
+            main: '#f50057',
+        },
+    },
+};
+
+const darkTheme = createTheme(darkThemeOptions);
+const lightTheme = createTheme(lightThemeOptions);
+
+const local_storage_theme_key = "ls_theme";
 
 export default function App() {
-  return (
-        <Box
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              minHeight: '100vh',
-            }}
-        >
-          <CssBaseline />
-          <Container component="main" sx={{ mt: 8, mb: 2 }} maxWidth="sm">
-            <Typography variant="h2" component="h1" gutterBottom>
-              HyperSynesthesia
-            </Typography>
-            <Typography variant="h5" component="h2" gutterBottom>
-              {'Xenon plugin / application for creating videos in response to audio'}
-            </Typography>
-            <Typography variant="body1">More information is coming soon.</Typography>
-          </Container>
-          <Box
-              component="footer"
-              sx={{
-                py: 3,
-                px: 2,
-                mt: 'auto',
-                backgroundColor: (theme) =>
-                    theme.palette.mode === 'light'
-                        ? theme.palette.grey[200]
-                        : theme.palette.grey[800],
-              }}
-          >
-            <Container maxWidth="sm">
-              <Typography variant="body1">
-                Copyright © Christopher Soderquist (Dakkra)
-              </Typography>
-            </Container>
-          </Box>
-        </Box>
-  );
+
+    const [isDark, setIsDark] = useState(localStorage.getItem(local_storage_theme_key));
+
+    function toggleTheme() {
+        const nVal = String(!JSON.parse(isDark))
+        setIsDark(nVal);
+        localStorage.setItem(local_storage_theme_key, nVal)
+    }
+
+    return (
+        <ThemeProvider theme={JSON.parse(isDark) ? darkTheme : lightTheme}>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    minHeight: '100vh',
+                }}
+            >
+                <CssBaseline/>
+                <Container component="main" sx={{mb: 2}} maxWidth="md">
+                    <Card>
+                        <Typography variant="h2" component="h1" gutterBottom>
+                            HyperSynesthesia
+                        </Typography>
+                        <Typography variant="h5" component="h2" gutterBottom>
+                            <Link href={"https://avereon.com/product/xenon"} target={"_blank"}>Xenon</Link> plugin /
+                            application for creating videos in response to audio
+                        </Typography>
+                    </Card>
+                    <Card>
+                        <Typography variant="body1">
+                            HyperSynthesia is an <Link href={"https://github.com/Dakkra/HyperSynesthesia"}
+                                                       target={"_blank"}>open source
+                            application</Link> created to be the spiritual successor to <Link
+                            href={"https://soniccandle.sourceforge.net/"} target={"_blank"}>Sonic Candle</Link>. The
+                            goal is
+                            to:
+                            <ul>
+                                <li> allow a user to
+                                    use any audio input file (via <Link href={"https://ffmpeg.org/"}
+                                                                        target={"_blank"}>FFMPEG</Link>)
+                                </li>
+                                <li>
+                                    generate frame data from audio input
+                                </li>
+                                <li>
+                                    analyze frame data to generate FFT, oscilloscope, and other audio visualization data
+                                </li>
+                                <li>
+                                    render using <Link href={"https://openjfx.io/"}
+                                                       target={"_blank"}>JavaFX</Link> (chosen
+                                    for web views to allow CSS styling of
+                                    components)
+                                </li>
+                                <li>
+                                    encode video using FFMPEG again
+                                </li>
+                            </ul>
+                            With these goals in mind, the project is using Xenon as an application framework to enable
+                            quality of life features such as auto-updates, threading, UI workflows, and more.
+                            <br/>
+                        </Typography>
+                    </Card>
+                    <Card>
+                        If you wish to join or watch the progress on this project, feel free to:
+                        <ul>
+                            <li>
+                                Join the <Link href={"https://discord.gg/MANggMn2aR"} target={"_blank"}>Discord</Link>
+                            </li>
+                            <li>
+                                Contribute to the <Link href={"https://github.com/Dakkra/HyperSynesthesia"}
+                                                        target={"_blank"}>project</Link> by making a pull request
+                            </li>
+                        </ul>
+                    </Card>
+                </Container>
+                <Box
+                    component="footer"
+                    sx={{
+                        py: 3,
+                        px: 2,
+                        mt: 'auto',
+                        backgroundColor: (theme) =>
+                            theme.palette.mode === 'light'
+                                ? theme.palette.grey[200]
+                                : theme.palette.grey[800],
+                    }}
+                >
+                    <Container maxWidth="sm">
+                        <Box sx={{display: "flex", flexDirection: "row", gap: "0.5cm"}}>
+                            <Typography variant="caption">
+                                <Link href={"https://github.com/Dakkra/HyperSynesthesia/blob/main/LICENSE"}
+                                      target={"_blank"}>HyperSynesthesia is under the GPL v3 License</Link>
+                                <br/>
+                                Copyright © Christopher Soderquist (<Link href={"https://dakkra.com"}
+                                                                          target={"_blank"}>Dakkra</Link>)
+                            </Typography>
+                            <br/>
+                            <Button onClick={() => toggleTheme()}>Toggle Light/Dark Mode</Button>
+                        </Box>
+                    </Container>
+                </Box>
+            </Box>
+        </ThemeProvider>
+    );
 }
