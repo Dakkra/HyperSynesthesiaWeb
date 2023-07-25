536b2093e979765fab27d5f79fb71f326d8d67a5
Chris Soderquist <contact@dakkra.com>
2023-07-25 00:11:48 -0600

    Update downloads page to use a table

--- hs-web/src/pages/DownloadsPage.jsx
+++ hs-web/src/pages/DownloadsPage.jsx
@@ -1,4 +1,4 @@
-import {Button, Card, Divider, Typography} from "@mui/material";
+import {Button, Card, Divider, TableBody, TableCell, TableContainer, TableRow, Typography} from "@mui/material";
 import * as React from "react";
 
 export default function DownloadsPage() {
@@ -8,31 +8,43 @@ export default function DownloadsPage() {
                 <Typography variant={"h4"}>Downloads</Typography>
                 <Divider></Divider>
                 <Typography variant={"h6"}>Latest Build Artifacts</Typography>
-                APPLICATION:
-                <Button disabled>Artifact</Button>
-                <Button color={"secondary"} disabled>Source</Button>
-                <Button disabled>Build History</Button>
-                <Divider></Divider>
-                PLUGIN:
-                <Button
-                    href={"https://nightly.link/Dakkra/HyperSynesthesia/workflows/ci/main/hypersynesthesia-plugin.jar.zip"}
-                    target={"_blank"}>Artifact</Button>
-                <Button color={"secondary"} href={"https://github.com/Dakkra/HyperSynesthesia"}
-                        target={"_blank"}>Source</Button>
-                <Button href={"https://github.com/Dakkra/HyperSynesthesia/actions?query=branch%3Amain+is%3Asuccess"}
-                        target={"_blank"}>
-                    Build History</Button>
-                <Divider></Divider>
-                WEB:
-                <Button
-                    href={"https://nightly.link/Dakkra/HyperSynesthesiaWeb/workflows/CI/main/hs-build.zip"}
-                    target={"_blank"}>Artifact</Button>
-                <Button color={"secondary"} href={"https://github.com/Dakkra/HyperSynesthesiaWeb"}
-                        target={"_blank"}>Source</Button>
-                <Button
-                    href={"https://github.com/Dakkra/HyperSynesthesiaWeb/actions?query=event%3Apush+is%3Asuccess+branch%3Amain"}
-                    target={"_blank"}>Build
-                    History</Button>
+                <TableContainer>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>APPLICATION</TableCell>
+                            <TableCell> <Button disabled>Artifact</Button></TableCell>
+                            <TableCell> <Button disabled>Source</Button></TableCell>
+                            <TableCell> <Button disabled>Build History</Button></TableCell>
+                        </TableRow>
+
+                        <TableRow>
+                            <TableCell>PLUGIN</TableCell>
+                            <TableCell> <Button
+                                href={"https://nightly.link/Dakkra/HyperSynesthesia/workflows/ci/main/hypersynesthesia-plugin.jar.zip"}
+                                target={"_blank"}>Artifact</Button></TableCell>
+                            <TableCell> <Button color={"secondary"} href={"https://github.com/Dakkra/HyperSynesthesia"}
+                                                target={"_blank"}>Source</Button></TableCell>
+                            <TableCell> <Button
+                                href={"https://github.com/Dakkra/HyperSynesthesia/actions?query=branch%3Amain+is%3Asuccess"}
+                                target={"_blank"}>
+                                Build History</Button></TableCell>
+                        </TableRow>
+
+                        <TableRow>
+                            <TableCell>WEB</TableCell>
+                            <TableCell> <Button
+                                href={"https://nightly.link/Dakkra/HyperSynesthesiaWeb/workflows/CI/main/hs-build.zip"}
+                                target={"_blank"}>Artifact</Button></TableCell>
+                            <TableCell> <Button color={"secondary"}
+                                                href={"https://github.com/Dakkra/HyperSynesthesiaWeb"}
+                                                target={"_blank"}>Source</Button></TableCell>
+                            <TableCell> <Button
+                                href={"https://github.com/Dakkra/HyperSynesthesiaWeb/actions?query=event%3Apush+is%3Asuccess+branch%3Amain"}
+                                target={"_blank"}>Build
+                                History</Button></TableCell>
+                        </TableRow>
+                    </TableBody>
+                </TableContainer>
             </Card>
         </>
     )
