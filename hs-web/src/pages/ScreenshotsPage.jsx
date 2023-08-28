04c4371c1f4e13b42f82572def448bc4f8fa06f8
Chris Soderquist <contact@dakkra.com>
2023-08-27 23:48:05 -0600

    New image viewer

--- hs-web/src/pages/ScreenshotsPage.jsx
+++ hs-web/src/pages/ScreenshotsPage.jsx
@@ -1,14 +1,20 @@
-import {Card, Typography} from "@mui/material";
+import {Card, ImageList, ImageListItem, Typography} from "@mui/material";
 import * as React from "react";
+import ModalImage from "react-modal-image";
 
 export default function ScreenshotsPage() {
     return (
         <Card>
             <Typography variant={"h4"}>Screenshots</Typography>
-            <img alt={"HyperSynesthesia About Screen"} style={{verticalAlign: "middle"}} src={"about_screen.png"}
-                 width={"100%"}/>
-            <img alt={"HyperSynesthesia Editor Screen"} style={{verticalAlign: "middle"}} src={"editor_screen.png"}
-                 width={"100%"}/>
+
+            <ImageList sx={{width: "100%"}} cols={2} rowHeight={200}>
+                <ImageListItem>
+                    <ModalImage small={"about_screen.png"} large={"about_screen.png"}></ModalImage>
+                </ImageListItem>
+                <ImageListItem>
+                    <ModalImage small={"editor_screen.png"} large={"editor_screen.png"}></ModalImage>
+                </ImageListItem>
+            </ImageList>
         </Card>
     )
 }
