4e0cc12e2644814862b7d54e2395a25452a93e0b
Chris Soderquist <contact@dakkra.com>
2023-08-27 22:08:22 -0600

    Add initial screenshots

--- hs-web/src/pages/ScreenshotsPage.jsx
+++ hs-web/src/pages/ScreenshotsPage.jsx
@@ -1,12 +1,14 @@
-import {Card, Divider, Typography} from "@mui/material";
+import {Card, Typography} from "@mui/material";
 import * as React from "react";
 
 export default function ScreenshotsPage() {
     return (
         <Card>
             <Typography variant={"h4"}>Screenshots</Typography>
-            <Divider></Divider>
-            Coming soon!
+            <img alt={"HyperSynesthesia About Screen"} style={{verticalAlign: "middle"}} src={"about_screen.png"}
+                 width={"100%"}/>
+            <img alt={"HyperSynesthesia Editor Screen"} style={{verticalAlign: "middle"}} src={"editor_screen.png"}
+                 width={"100%"}/>
         </Card>
     )
 }
