9862d79f2994aa285127e08553196700a919a274
Chris Soderquist <contact@dakkra.com>
2023-12-26 01:13:45 -0700

    Update ScreenshotsPage layout and content
    The layout styling of ScreenshotsPage has been improved to enhance visual presentation. Additionally, a new YouTube iframe has been added, increasing the number of embedded videos on the page. The "Typography" import was removed as it's no longer needed.

--- hs-web/src/pages/ScreenshotsPage.jsx
+++ hs-web/src/pages/ScreenshotsPage.jsx
@@ -1,12 +1,16 @@
-import {Card, Typography} from "@mui/material";
+import {Card} from "@mui/material";
 import * as React from "react";
 
 export default function ScreenshotsPage() {
     return (
-        <Card>
-            <Typography variant={"h4"}>Screenshots</Typography>
+        <Card sx={{display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center", gap: 4}}>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/YByR2u-lFKA?si=c5xIZhOCQb6LiL60"
                     title="YouTube video player" frameBorder={0}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowFullScreen></iframe>
+
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/UmcQK385Ooo?si=FkaOKb6jp-rUUE9Q"
+                    title="YouTube video player" frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                     allowFullScreen></iframe>
         </Card>
